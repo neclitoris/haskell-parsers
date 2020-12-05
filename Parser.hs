a9be2d253b85a347b4a3cdd504fed6527d04e4be
Nikita Solodovnikov <nas140301@gmail.com>
2020-12-05 21:55:02 +0300

    less general

--- Parser.hs
+++ Parser.hs
@@ -6,7 +6,6 @@
 module Parser
   ( ParserC,
     runParserC,
-    Parser (..),
     string,
     pEOF,
     module Control.Applicative,
@@ -21,23 +20,16 @@ import Data.List
 import Data.Monoid
 import Token
 
--- we should be able to construct parsers matching a certain symbol
-class Parser parser sym tok | parser sym -> tok where
-  symbol :: sym -> parser tok
-  symbolIs :: (sym -> Bool) -> parser tok
-
-class ParserEOF parser where
-  pEOF :: parser ()
 
 -- Simple concrete parser type. Takes enclosing monad, token type
 -- and result type as parameters.
-newtype ParserC s m t a = ParserC {runParserC :: s -> m [(a, s)]}
+newtype ParserC s m a = ParserC {runParserC :: s -> m [(a, s)]}
 
-instance Functor m => Functor (ParserC s m t) where
+instance Functor m => Functor (ParserC s m) where
   -- simply apply f to parsing result
   fmap f (ParserC xs) = ParserC (fmap (\l -> [(f a, s') | (a, s') <- l]) . xs)
 
-instance Monad m => Applicative (ParserC s m t) where
+instance Monad m => Applicative (ParserC s m) where
   -- parse nothing, return m
   pure m = ParserC $ \s -> pure [(m, s)]
 
@@ -46,7 +38,7 @@ instance Monad m => Applicative (ParserC s m t) where
     xs <- f s
     (fmap join . sequenceA) [let a' (x, s) = (a x, s) in fmap (fmap a') (g s') | (a, s') <- xs]
 
-instance Monad m => Alternative (ParserC s m t) where
+instance Monad m => Alternative (ParserC s m) where
   -- failed parse
   empty = ParserC $ const $ pure []
 
@@ -55,32 +47,23 @@ instance Monad m => Alternative (ParserC s m t) where
   -- for List this will concatenate lists (yielding all possible parses so far)
   ParserC f <|> ParserC g = ParserC $ \s -> (++) <$> f s <*> g s
 
-instance Monad m => Monad (ParserC s m t) where
+instance Monad m => Monad (ParserC s m) where
   -- run ma, then apply f to the result and use resulting parser for further parsing
   -- this allows to parse non-context-free languages
   ParserC ma >>= f = ParserC $ \s -> do
     l <- ma s
     (fmap join . sequenceA) $ [runParserC (f a) s' | (a, s') <- l]
 
-instance
-  ( Token symbol token,
-    Stream state token,
-    Applicative monad
-  ) =>
-  Parser (ParserC state monad token) symbol token
-  where
-  symbolIs p = ParserC $ \str -> case suncons str of
-    Just (c, str') | symIs p c -> pure [(c, str')]
-    _ -> pure []
-  symbol c = symbolIs (== c)
+symbolIs :: (Token sym t, Stream s t, Applicative m) => (sym -> Bool) -> ParserC s m t
+symbolIs p = ParserC $ \str -> case suncons str of
+  Just (c, str') | symIs p c -> pure [(c, str')]
+  _ -> pure []
 
-instance
-  ( Stream state token,
-    Applicative monad
-  ) =>
-  ParserEOF (ParserC state monad token)
-  where
-  pEOF = ParserC $ \str -> if eof str then pure [((), str)] else pure []
+symbol :: (Token sym t, Stream s t, Applicative m) => sym -> ParserC s m t
+symbol c = symbolIs (== c)
 
-string :: (Token sym t, Parser p sym t, Applicative p) => [sym] -> p [t]
+pEOF :: (Stream s t, Applicative m) => ParserC s m ()
+pEOF = ParserC $ \str -> if eof str then pure [((), str)] else pure []
+
+string :: (Token sym t, Stream s t, Monad m) => [sym] -> ParserC s m [t]
 string = traverse symbol
